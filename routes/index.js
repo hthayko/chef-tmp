e998e8a82371a0dd54ddabd10e4cc36d813dd770
hthayko <tehayk@stanford.edu>
2018-04-24 22:57:09 -0700

    added demo for vue

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chef++' });
 });
 
+router.get('/demo', function(req, res, next) {
+  res.render('tutorial-demo-v1');
+});
+
 module.exports = router;
